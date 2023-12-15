cb57638ac3f68baaa3e9f52be8fb2d479e87b0c9
dave502 <davtyan.d.n@mail.ru>
2023-12-15 04:21:04 +0200

    added limit to 5 news

--- web/news/views.py
+++ web/news/views.py
@@ -12,10 +12,10 @@ mongo_client = pymongo.MongoClient(mongo_url)
 db = mongo_client[mongo_db]
 mongo_collection = db['news_spider']
 
-# Create your views here.
+
 def index(request):
     template = loader.get_template("news/index.html")
-    news = mongo_collection.find({}).sort({created: -1}).limit(5)
+    news = mongo_collection.find({}).sort({"created": -1}).limit(5)
     context = {"news": news}
     print("news count", len(news))
     return render(request, "news/index.html", context)
